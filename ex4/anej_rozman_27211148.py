2917660965d0a5ba5cec37273e39ef7352a03443
Anej Rozman <anej.roz02@gmail.com>
2024-01-23 12:59:09 +0100

    solution

--- ex4/anej_rozman_27211148.py
+++ ex4/anej_rozman_27211148.py
@@ -7,16 +7,60 @@ import sys
 
 #------------------------------------------------------------------------------#
 
+M = 10 ** 9 + 7
 def solve(s):
-    if len(s) == 0:
-        return 0
-    res = 9 if s[0] == '*' else 1
-    prev = s[0]
-    for i in range(1, len(s)):
-
-        prev = s[i]
-        
-    print(res % (10^9 + 7))
+    first = 1
+    second = 9 if(s[0] == '*') else(0 if(s[0] == '0') else 1)
+ 
+    for i in range(1,len(s)):
+ 
+        temp = second
+ 
+        # If s[i] == '*' there can be
+        # 9 possible values of *
+        if (s[i] == '*'):
+            second = 9 * second
+ 
+            # If previous character is 1
+            # then words that can be formed
+            # are K(11), L(12), M(13), N(14)
+            # O(15), P(16), Q(17), R(18), S(19)
+            if (s[i - 1] == '1'):
+                second = (second + 9 * first) % M
+ 
+            # If previous character is 2
+            # then the words that can be formed
+            # are U(21), V(22), W(23), X(24)Y(25), Z(26)
+            elif (s[i - 1] == '2'):
+                second = (second + 6 * first) % M
+ 
+            # If the previous digit is * then
+            # all 15 2- digit characters can be
+            # formed
+            elif (s[i - 1] == '*'):
+                second = (second + 15 * first) % M
+         
+        # If s[i] != '*'
+        else:
+ 
+            second = second if(s[i] != '0') else 0
+ 
+            # Adding first in second
+            # if s[i-1]=1
+            if (s[i - 1] == '1'):
+                second = (second + first) % M
+ 
+            # Adding first in second if
+            # s[i-1] == 2 and s[i]<=l           
+            elif (s[i - 1] == '2' and s[i] <= '6'):
+                second = (second + first) % M
+ 
+            # if s[i-1] == '*' the union
+            # of above 2 cases has to be done
+            elif (s[i - 1] == '*'):
+                second = (second + (2 if(s[i] <= '6') else 1) * first) % M
+        first = temp
+    print(second)
 
 #------------------------------------------------------------------------------#
 
